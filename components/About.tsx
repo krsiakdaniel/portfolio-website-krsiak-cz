70c9994386ede61557255cb5c0234e4abd8c92ce
Krsiak Daniel <krsiak.daniel@gmail.com>
2024-02-23 17:32:51 +0100

    fix: revert avif
    Netlify does not show it on deploy

--- components/About.tsx
+++ components/About.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import krsiak from '../public/images/krsiak.avif'
+import krsiak from '../public/images/krsiak.jpg'
 
 const About = () => {
   const experience = [
