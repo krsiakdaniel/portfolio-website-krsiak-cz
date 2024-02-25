2e57bc6700718ff9ebdea47960971f5b794d49fc
Krsiak Daniel <krsiak.daniel@gmail.com>
2024-02-26 00:41:34 +0100

    feat: mini footer

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -4,6 +4,23 @@ import phone from '../public/icons/png/phone.png'
 import gmail from '../public/icons/png/gmail.png'
 import linkedin from '../public/icons/png/linkedin.png'
 
+interface Contact {
+  emoji: string
+  text: string
+  href: string
+}
+
+const contacts: Contact[] = [
+  { emoji: '📱', text: '(+420) 734 496 308', href: 'tel:+420734496308' },
+  { emoji: '📧', text: 'krsiak.daniel@gmail.com', href: 'mailto:krsiak.daniel@gmail.com' },
+  {
+    emoji: '💬',
+    text: 'LinkedIn →',
+    href: 'https://www.linkedin.com/in/krsiakdaniel/',
+  },
+]
+
+// TODO: map contacts data
 const ContactMe = () => {
   return (
     <div className="container mx-auto">
@@ -15,51 +32,51 @@ const ContactMe = () => {
       </div>
       <div className="mb-20">
         <div className="flex flex-col lg:flex-row lg:space-x-10">
-          <div className="w-full max-w-3xl lg:max-w-sm bg-white border border-neutral-400 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mt-8">
+          <div className="w-full max-w-3xl lg:max-w-sm bg-white border border-neutral-400 rounded-lg shadow mt-8">
             <div className="flex justify-end px-4 pt-8"></div>
             <div className="flex flex-col items-center pb-10">
               <Image className="w-24 h-24 mb-3 rounded-full shadow-lg" src={phone} alt="phone" />
               <h5 className="mb-1 text-xl font-medium text-gray-900 uppercase">Phone</h5>
-              <span className="text-sm text-gray-500 dark:text-gray-400">Time zone GMT+1</span>
+              <span className="text-sm text-gray-500 text-neutral-600">Time zone GMT+1</span>
               <div className="flex mt-4 md:mt-6">
                 <a
                   href="tel:+420734496308"
                   target="_blank"
-                  className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-primary-500 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-primary-500 rounded-lg hover:opacity-90 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                   (+420) 734 496 308
                 </a>
               </div>
             </div>
           </div>
-          <div className="w-full max-w-3xl lg:max-w-sm bg-white border border-neutral-400 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mt-8">
+          <div className="w-full max-w-3xl lg:max-w-sm bg-white border border-neutral-400 rounded-lg shadow mt-8">
             <div className="flex justify-end px-4 pt-8"></div>
             <div className="flex flex-col items-center pb-10">
               <Image className="w-24 h-24 mb-3 rounded-full shadow-lg" src={gmail} alt="email" />
               <h5 className="mb-1 text-xl font-medium text-gray-900 uppercase">Email</h5>
-              <span className="text-sm text-gray-500 dark:text-gray-400">I read it daily</span>
+              <span className="text-sm text-gray-500 text-neutral-600">I read it daily</span>
               <div className="flex mt-4 md:mt-6">
                 <a
                   href="mailto:krsiak.daniel@gmail.com"
                   target="_blank"
-                  className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-primary-500 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-primary-500 rounded-lg hover:opacity-90 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                   krsiak.daniel@gmail.com
                 </a>
               </div>
             </div>
           </div>
-          <div className="w-full max-w-3xl lg:max-w-sm bg-white border border-neutral-400 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mt-8">
+          <div className="w-full max-w-3xl lg:max-w-sm bg-white border border-neutral-400 rounded-lg shadow mt-8">
             <div className="flex justify-end px-4 pt-8"></div>
             <div className="flex flex-col items-center pb-10">
               <Image className="w-24 h-24 mb-3 rounded-full shadow-lg" src={linkedin} alt="linkedin" />
               <h5 className="mb-1 text-xl font-medium text-gray-900 uppercase">LinkedIn</h5>
-              <span className="text-sm text-gray-500 dark:text-gray-400">Easy and fast</span>
+              <span className="text-sm text-gray-500 text-neutral-600">Easy and fast</span>
               <div className="flex mt-4 md:mt-6">
                 <a
                   href="https://www.linkedin.com/in/krsiakdaniel/"
                   target="_blank"
-                  className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-primary-500 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-primary-500 rounded-lg hover:opacity-90 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                   Send me message
                 </a>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,45 +2,10 @@ import React from 'react'
 import Link from 'next/link'
 
 const Footer = () => {
-  interface Contact {
-    emoji: string
-    text: string
-    href: string
-  }
-
-  const contacts: Contact[] = [
-    { emoji: '📱', text: '(+420) 734 496 308', href: 'tel:+420734496308' },
-    { emoji: '📧', text: 'krsiak.daniel@gmail.com', href: 'mailto:krsiak.daniel@gmail.com' },
-    {
-      emoji: '💬',
-      text: 'LinkedIn →',
-      href: 'https://www.linkedin.com/in/krsiakdaniel/',
-    },
-  ]
   return (
-    <footer id="contact" className="bg-neutral-900 text-white">
+    <footer id="contact" className="">
       <div className="container mx-auto">
-        <div className="flex">
-          <div>
-            <h3 className="md:text-display-lg text-display-md font-semibold pt-8 text-white uppercase">Contact me</h3>
-            <p className="text-body-md font-normal text-white mb-8">
-              You can call me, send email or chat with me on LinkedIn.
-            </p>
-            <div className="mb-8">
-              <ul>
-                {contacts.map((item) => (
-                  <li className="text-body-md font-normal" key={item.href}>
-                    {item.emoji}{' '}
-                    <a href={item.href} className="hover:text-primary-500 underline hover:no-underline">
-                      {item.text}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="pb-8">
+        <div className="flex py-4 border-t border-dashed border-neutral-300">
           <p>
             Copyright © 2024 |{' '}
             <Link href="/" className="hover:text-primary-500 underline hover:no-underline">
