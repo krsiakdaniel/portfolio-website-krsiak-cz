932d29d8680387c30d73cb28c6b1b92e7ce6a5f6
Krsiak Daniel <krsiak.daniel@gmail.com>
2024-02-26 01:34:42 +0100

    ref: getCurrentYear()

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Link from 'next/link'
 
+const getCurrentYear = () => {
+  return new Date().getFullYear()
+}
+
 const Footer = () => {
   return (
     <footer id="contact" className="">
       <div className="container mx-auto">
         <div className="flex py-4 border-t border-dashed border-neutral-300">
           <p>
-            Copyright © 2024 |{' '}
+            Copyright © {getCurrentYear()} |{' '}
             <Link href="/" className="hover:text-primary-500 underline hover:no-underline">
               krsiak.cz
             </Link>
