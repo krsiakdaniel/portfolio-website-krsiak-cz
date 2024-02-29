574b76decad5416fcc72cef18c65979261fff67e
Krsiak Daniel <krsiak.daniel@gmail.com>
2024-02-29 16:06:04 +0100

    css: header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,6 @@ import Link from 'next/link'
 
 import { LINK_LINKEDIN } from '@/utils/constants'
 
-// FIXME: primary -> purple
 const Header = () => {
   return (
     <header className="sticky top-0 bg-white z-20 border-b border-neutral-400">
@@ -10,7 +9,7 @@ const Header = () => {
         <div className="flex py-4 justify-between items-center">
           <div className="flex flex-row gap-8 items-center">
             <Link href="/">
-              <p className="text-body-md font-bold text-neutral-700 hover:text-primary-500 px-4 cursor-pointer">
+              <p className="text-body-md font-bold text-neutral-700 hover:text-purple-500 px-4 cursor-pointer">
                 krsiak.cz
               </p>
             </Link>
@@ -19,7 +18,7 @@ const Header = () => {
             <div className="md:flex flex-row gap-6 items-center">
               <Link
                 href={LINK_LINKEDIN}
-                className="text-body-md font-bold uppercase text-neutral-700 hover:text-primary-500 px-4 cursor-pointer"
+                className="text-body-md font-bold uppercase text-neutral-700 hover:text-purple-500 px-4 cursor-pointer"
               >
                 LinkedIn →
               </Link>
