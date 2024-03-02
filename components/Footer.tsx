458923224bf2986cdd5fdff7be8a2c76d62842b5
Krsiak Daniel <krsiak.daniel@gmail.com>
2024-03-03 00:32:30 +0100

    feat: footer content

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,13 +10,28 @@ const Footer = () => {
   return (
     <PageContainer id={ID_FOOTER}>
       <footer className="bg-white dark:bg-gray-800 border-t border-dashed">
-        <div className="w-full mx-auto py-4 md:flex md:items-center md:justify-between text-center">
-          <p className="text-gray-400 dark:text-gray-300">
-            Copyright © {getCurrentYear()} |{' '}
+        <div className="w-full mx-auto py-4 flex flex-col md:flex-row md:gap-2 items-center text-gray-400 dark:text-gray-300">
+          <span>
+            Made in{' '}
+            <a
+              href="https://maps.app.goo.gl/imBSG9zMJPpRJeyZA"
+              target="_blank"
+              className="hover:text-purple-500 underline hover:no-underline"
+            >
+              Czech Republic
+            </a>{' '}
+            🇨🇿
+          </span>
+          <span className="hidden md:inline-flex">|</span>
+          <span>Helped by 2 cats 😼 😺</span>
+          <span className="hidden md:inline-flex">|</span>
+          <span>Copyright © {getCurrentYear()}</span>
+          <span className="hidden md:inline-flex">|</span>
+          <span>
             <Link href="/" className="hover:text-purple-500 underline hover:no-underline">
               krsiak.cz
             </Link>
-          </p>
+          </span>
         </div>
       </footer>
     </PageContainer>
