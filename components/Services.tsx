147ea543053e04abdb731a62aca157d625c4403b
Krsiak Daniel <krsiak.daniel@gmail.com>
2024-03-04 09:37:15 +0100

    fix: &nbsp; for text to display fine on mobile

--- components/Services.tsx
+++ components/Services.tsx
@@ -10,7 +10,7 @@ const Services = () => {
     <PageContainer id={ID_SERVICES}>
       <div className="mb-20 flex flex-col rounded-lg border border-dashed bg-green-100 px-5 py-10 lg:flex-row">
         <div className="flex flex-col justify-center py-10 text-center lg:w-1/2 lg:px-10 lg:text-left">
-          <h2 className="text-5xl font-bold uppercase dark:text-white">What I do</h2>
+          <h2 className="text-5xl font-bold uppercase dark:text-white">What I&nbsp;do</h2>
           <p className="mb-4 text-lg font-normal text-neutral-600 dark:text-neutral-600 lg:max-w-[550px]">
             I am passionate about creating high-quality products that meet the needs and expectations both of the client
             and the users.
