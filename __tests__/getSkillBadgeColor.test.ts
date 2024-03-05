bff8d47873fe0196b2d2ce25873549e5eff7934c
Krsiak Daniel <krsiak.daniel@gmail.com>
2024-03-05 13:20:58 +0100

    test: add 2 more unit test or badge colors

--- __tests__/getSkillBadgeColor.test.ts
+++ __tests__/getSkillBadgeColor.test.ts
@@ -5,4 +5,12 @@ describe('getSkillBadgeColor', () => {
   it(`returns 'yellow' color for 'Frontend'`, () => {
     expect(getSkillBadgeColor('Frontend' as CategoryDescriptionType)).toEqual('yellow')
   })
+
+  it(`returns 'indigo' color for 'Design'`, () => {
+    expect(getSkillBadgeColor('Design' as CategoryDescriptionType)).toEqual('indigo')
+  })
+
+  it(`return 'lime' color for 'Other'`, () => {
+    expect(getSkillBadgeColor('Other' as CategoryDescriptionType)).toEqual('lime')
+  })
 })
