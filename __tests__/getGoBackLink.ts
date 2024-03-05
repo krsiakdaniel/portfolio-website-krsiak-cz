80e0ef2b92aa281ba0689dae07166e29500f35a8
Krsiak Daniel <krsiak.daniel@gmail.com>
2024-03-05 14:00:33 +0100

    test: unit test - getGoBackLinkID()

---
+++ __tests__/getGoBackLink.ts
@@ -0,0 +1,15 @@
+import { ID_PROJECTS_PERSONAL, ID_PROJECTS_WORK } from '@/utils/constants'
+import { getGoBackLinkID } from '@/utils/getGoBackLink'
+import { GoBackLinkType } from '@/utils/types'
+
+describe('getGoBackLinkID', () => {
+  it('returns correct link for type "work"', () => {
+    const sectionID = 'react'
+    expect(getGoBackLinkID('work' as GoBackLinkType, sectionID)).toEqual(`/#${ID_PROJECTS_WORK}-${sectionID}`)
+  })
+
+  it('returns correct link for type "personal"', () => {
+    const sectionID = 'next'
+    expect(getGoBackLinkID('personal' as GoBackLinkType, sectionID)).toEqual(`/#${ID_PROJECTS_PERSONAL}-${sectionID}`)
+  })
+})
