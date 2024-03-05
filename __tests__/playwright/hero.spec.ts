69a8a4543e0262c7f300da40e8b51e372cc15eb7
Krsiak Daniel <krsiak.daniel@gmail.com>
2024-03-06 00:51:56 +0100

    test: E2E - hero - link

--- __tests__/playwright/hero.spec.ts
+++ __tests__/playwright/hero.spec.ts
@@ -1,4 +1,6 @@
-import { test, expect, Browser, BrowserContext, Page, chromium } from '@playwright/test'
+import { Browser, BrowserContext, Page, chromium, expect, test } from '@playwright/test'
+
+import { LINK_GITHUB, LINK_LINKEDIN, LINK_RESUME } from '@/utils/constants'
 
 let browser: Browser
 let context: BrowserContext
@@ -22,7 +24,7 @@ test.afterEach(async () => {
   await context.close()
 })
 
-test.describe('Hero component', () => {
+test.describe('Hero - Heading and texts', () => {
   test('should render the h1 heading correctly', async () => {
     const h1Text = await page.textContent('[data-testid="hero-heading"]')
     expect(h1Text).toContain('Daniel Krsiak')
@@ -34,5 +36,25 @@ test.describe('Hero component', () => {
     const paragraph2Text = await page.textContent('[data-testid="hero-paragraph-2"]')
     expect(paragraph1Text).toContain('Hi 👋 I am React Developer based in Brno, Czech Republic 🇨🇿')
     expect(paragraph2Text).toContain('I enjoy working with JavaScript, TypeScript, and React.')
+  })
+})
+
+test.describe('Hero - Links', () => {
+  test('should have the correct LinkedIn link', async () => {
+    await page.goto('http://localhost:3000') // replace with  app's url
+    const linkedinLink = await page.getAttribute('[data-testid="hero-link-linkedin"]', 'href')
+    expect(linkedinLink).toBe(LINK_LINKEDIN)
+  })
+
+  test('should have the correct GitHub link', async () => {
+    await page.goto('http://localhost:3000') // replace with  app's url
+    const githubLink = await page.getAttribute('[data-testid="hero-link-github"]', 'href')
+    expect(githubLink).toBe(LINK_GITHUB)
+  })
+
+  test('should have the correct Resume link', async () => {
+    await page.goto('http://localhost:3000') // replace with  app's url
+    const resumeLink = await page.getAttribute('[data-testid="hero-link-resume"]', 'href')
+    expect(resumeLink).toBe(LINK_RESUME)
   })
 })
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -37,6 +37,7 @@ const Hero = () => {
 
       <div className="mt-8 flex flex-col items-center justify-center sm:flex-row">
         <a
+          data-testid="hero-link-linkedin"
           href={LINK_LINKEDIN}
           target="_blank"
           className="text-md mb-2 w-40 rounded-lg bg-blue-700 px-5 py-2.5 text-center font-medium uppercase text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:mb-0 sm:mr-2"
@@ -44,6 +45,7 @@ const Hero = () => {
           LinkedIn
         </a>
         <a
+          data-testid="hero-link-github"
           href={LINK_GITHUB}
           target="_blank"
           className="text-md mb-2 w-40 rounded-lg bg-gray-700 px-5 py-2.5 text-center font-medium uppercase text-white hover:bg-gray-800 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800 sm:mb-0 sm:mr-2"
@@ -51,6 +53,7 @@ const Hero = () => {
           GitHub
         </a>
         <a
+          data-testid="hero-link-resume"
           href={LINK_RESUME}
           target="_blank"
           className="text-md w-40 rounded-lg bg-red-700 px-5 py-2.5 text-center font-medium uppercase text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800 sm:mb-0"
