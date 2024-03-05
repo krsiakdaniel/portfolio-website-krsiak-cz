f417ce8802595f2f6a9f92fc427a5f8ae3fbebfe
Krsiak Daniel <krsiak.daniel@gmail.com>
2024-03-05 13:48:23 +0100

    ref: formatting

--- __tests__/getSkillBadgeColor.test.ts
+++ __tests__/getSkillBadgeColor.test.ts
@@ -2,15 +2,15 @@ import { getSkillBadgeColor } from '@/utils/getSkillBadgeColor'
 import { CategoryDescriptionType } from '@/utils/types'
 
 describe('getSkillBadgeColor', () => {
-  it(`returns 'yellow' color for 'Frontend'`, () => {
+  it('returns "yellow" color for "Frontend"', () => {
     expect(getSkillBadgeColor('Frontend' as CategoryDescriptionType)).toEqual('yellow')
   })
 
-  it(`returns 'indigo' color for 'Design'`, () => {
+  it('returns "indigo" color for "Design"', () => {
     expect(getSkillBadgeColor('Design' as CategoryDescriptionType)).toEqual('indigo')
   })
 
-  it(`return 'lime' color for 'Other'`, () => {
+  it('return "lime" color for "Other"', () => {
     expect(getSkillBadgeColor('Other' as CategoryDescriptionType)).toEqual('lime')
   })
 })
