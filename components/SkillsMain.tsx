91fd4c26e4a5f5b77a8e587e871205da3818ebc9
Krsiak Daniel <krsiak.daniel@gmail.com>
2024-03-07 14:22:38 +0100

    ref: code sections for skills responsive

--- components/SkillsMain.tsx
+++ components/SkillsMain.tsx
@@ -5,49 +5,59 @@ import react from '@/public/icons/svg/skills/react.svg'
 import ts from '@/public/icons/svg/skills/ts.svg'
 import { ID_SKILLS_MAIN } from '@/utils/constants'
 
+const SkillsSection1 = () => {
+  return (
+    <div className="mb-4 flex justify-center md:mb-8 md:items-center lg:mb-0 lg:mr-8">
+      <Card
+        src={js}
+        alt="JS"
+        title="JavaScript"
+        titleHighlight="5 years"
+        description="Essential for creating modern interactive web applications."
+      />
+    </div>
+  )
+}
+
+const SkillsSection2 = () => {
+  return (
+    <div className="flex flex-col items-center justify-center md:flex-row lg:mr-0 lg:flex-col">
+      <div className="mb-4 mr-0 md:mb-0 md:mr-8 lg:mb-8 lg:mr-0">
+        <Card
+          src={ts}
+          alt="TS"
+          title="TypeScript"
+          titleHighlight="3 years"
+          description="Ensuring type safety, enhancing code reliability and scalability."
+        />
+      </div>
+      <div>
+        <Card
+          src={react}
+          alt="React"
+          title="React"
+          titleHighlight="4 years"
+          description="Great for components that are reusable and easy to maintain."
+        />
+      </div>
+    </div>
+  )
+}
+
 const SkillsMain = () => {
   return (
     <PageContainer id={ID_SKILLS_MAIN}>
-      <div className="mb-20 flex flex-col rounded-lg border border-dashed bg-green-100 px-5 py-10 lg:flex-row">
-        <div className="flex flex-col justify-center py-10 text-center lg:w-1/2 lg:px-10 lg:text-left">
+      <div className="mb-20 flex flex-col rounded-lg border border-dashed bg-green-100 px-10 py-10 lg:flex-row">
+        <div className="mb-10 flex flex-col justify-center pt-10 text-center lg:mb-0 lg:w-1/2 lg:pl-5 lg:pr-10 lg:pt-0 lg:text-left">
           <h2 className="text-5xl font-bold uppercase dark:text-white">What I&nbsp;do</h2>
-          <p className="mb-4 text-lg font-normal text-neutral-600 dark:text-neutral-600 lg:max-w-[550px]">
+          <p className="text-lg font-normal text-neutral-600 dark:text-neutral-600 lg:max-w-[550px]">
             I am passionate about creating high-quality products that meet the needs and expectations both of the client
             and the users.
           </p>
         </div>
 
-        <div className="mb-4 flex justify-center md:items-center lg:mb-0 lg:mr-8">
-          <Card
-            src={js}
-            alt="JS"
-            title="JavaScript"
-            titleHighlight="5 years"
-            description="Essential for creating modern interactive web applications."
-          />
-        </div>
-
-        <div className="flex flex-col items-center lg:mr-10">
-          <div className="mb-4 lg:mb-8">
-            <Card
-              src={ts}
-              alt="TS"
-              title="TypeScript"
-              titleHighlight="3 years"
-              description="Ensuring type safety, enhancing code reliability and scalability."
-            />
-          </div>
-
-          <div>
-            <Card
-              src={react}
-              alt="React"
-              title="React"
-              titleHighlight="4 years"
-              description="Great for components that are reusable and easy to maintain."
-            />
-          </div>
-        </div>
+        <SkillsSection1 />
+        <SkillsSection2 />
       </div>
     </PageContainer>
   )
