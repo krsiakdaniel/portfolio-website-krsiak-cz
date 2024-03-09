51045b606c6c56f5c3bf53c77347b59e3c4ab08e
Krsiak Daniel <krsiak.daniel@gmail.com>
2024-03-09 02:05:16 +0100

    ref: code quality

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -4,26 +4,32 @@ import { iconsSkills1, iconsSkills2, iconsSkills3 } from '@/data/skills/skills-m
 import { ID_SKILLS } from '@/utils/constants'
 import Image from 'next/image'
 
+interface Icon {
+  name: string
+  path: string
+}
+
+interface SkillsIconGroupProps {
+  icons: Icon[]
+  className?: string
+}
+
+const SkillsIconGroup = ({ icons, className = '' }: SkillsIconGroupProps) => (
+  <div className={`flex justify-center ${className}`}>
+    {icons.map((item) => (
+      <Image key={item.name} src={item.path} alt={item.name} className="mr-2" width={48} height={48} />
+    ))}
+  </div>
+)
+
 const Skills = () => {
   return (
     <PageContainer id={ID_SKILLS}>
       <DividerWithText text="Skills" />
       <div className="mt-5 flex flex-col justify-center md:flex-row">
-        <div className="mb-4 flex justify-center md:mb-0">
-          {iconsSkills1.map((item) => {
-            return <Image key={item.name} src={item.path} alt={item.name} className="mr-2" width={48} height={48} />
-          })}
-        </div>
-        <div className="mb-4 flex justify-center md:mb-0">
-          {iconsSkills2.map((item) => {
-            return <Image key={item.name} src={item.path} alt={item.name} className="mr-2" width={48} height={48} />
-          })}
-        </div>
-        <div className="flex justify-center">
-          {iconsSkills3.map((item) => {
-            return <Image key={item.name} src={item.path} alt={item.name} className="mr-2" width={48} height={48} />
-          })}
-        </div>
+        <SkillsIconGroup icons={iconsSkills1} className="mb-4 md:mb-0" />
+        <SkillsIconGroup icons={iconsSkills2} className="mb-4 md:mb-0" />
+        <SkillsIconGroup icons={iconsSkills3} />
       </div>
       <div>
         <p className="text-body-sm mt-4 text-center italic text-neutral-500">
