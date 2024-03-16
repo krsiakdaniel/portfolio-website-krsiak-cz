b709a2ef56bc863b1b83338eb21bc9060f172c1f
Krsiak Daniel <krsiak.daniel@gmail.com>
2024-03-16 21:05:39 +0100

    ref: pages files

--- app/personal-projects/page.tsx
+++ app/personal-projects/page.tsx
@@ -1,7 +1,38 @@
-import ProjectsPersonal from '@/components/projects/ProjectsPersonal'
+import PageContainer from '@/components/layout/PageContainer'
+import ProjectsLayout from '@/components/layout/ProjectsLayout'
+import { ProjectSection } from '@/components/projects/ProjectSection'
+import {
+  projectsPersonalNext,
+  projectsPersonalReact,
+  projectsPersonalVue,
+} from '@/data/projects/personal/projectsOverview'
+import { ID_PROJECTS_PERSONAL, ID_SECTION_NEXT, ID_SECTION_REACT, ID_SECTION_VUE } from '@/utils/constants'
 
-const Personal = () => {
-  return <ProjectsPersonal />
+const ProjectsPersonal = () => {
+  return (
+    <PageContainer id={ID_PROJECTS_PERSONAL}>
+      <ProjectsLayout
+        heading="Personal Projects"
+        description="I like to learn new technologies and create personal projects where I can apply what I know and keep up on the latest trends."
+      >
+        <ProjectSection
+          sectionId={`${ID_PROJECTS_PERSONAL}-${ID_SECTION_NEXT}`}
+          sectionText="Next"
+          projectData={projectsPersonalNext}
+        />
+        <ProjectSection
+          sectionId={`${ID_PROJECTS_PERSONAL}-${ID_SECTION_REACT}`}
+          sectionText="React"
+          projectData={projectsPersonalReact}
+        />
+        <ProjectSection
+          sectionId={`${ID_PROJECTS_PERSONAL}-${ID_SECTION_VUE}`}
+          sectionText="Vue"
+          projectData={projectsPersonalVue}
+        />
+      </ProjectsLayout>
+    </PageContainer>
+  )
 }
 
-export default Personal
+export default ProjectsPersonal
