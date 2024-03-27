13164675f90cc0225b2ff0085be5a726bc234021
Krsiak Daniel <krsiak.daniel@gmail.com>
2024-03-27 23:43:55 +0100

    install: vercel - analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Analytics } from '@vercel/analytics/react';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-import Footer from '@/components/Footer'
-import Header from '@/components/header/Header'
+import Footer from '@/components/Footer';
+import Header from '@/components/header/Header';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,6 +27,7 @@ export default function RootLayout({
           <main>{children}</main>
         </div>
         <Footer />
+        <Analytics />
       </body>
     </html>
   )
