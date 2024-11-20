8751a9b10388416ad7e6ca5a0544a1d9e4e72d58
Krsiak Daniel <krsiak.daniel@gmail.com>
2024-11-20 13:29:32 +0100

    fix: replace A with LINK

--- components/shared/Breadcrumbs.tsx
+++ components/shared/Breadcrumbs.tsx
@@ -1,5 +1,6 @@
 import { IconCaretRight, IconHome } from '@/components/icons'
 import { BreadCrumbsType } from '@/utils/types'
+import Link from 'next/link'
 
 const BreadCrumbs = ({ linkLevel1, textLevel1, linkLevel2, textLevel2 }: BreadCrumbsType) => {
   return (
@@ -7,21 +8,24 @@ const BreadCrumbs = ({ linkLevel1, textLevel1, linkLevel2, textLevel2 }: BreadCr
       <nav className="mb-10 flex text-neutral-700" aria-label="Breadcrumbs" data-testid="breadcrumbs">
         <ol className="inline-flex flex-row flex-wrap items-start space-x-1 md:space-x-2">
           <li className="inline-flex items-center">
-            <a href="/" className="inline-flex items-center text-sm font-medium text-neutral-700 hover:text-violet-600">
+            <Link
+              href="/"
+              className="inline-flex items-center text-sm font-medium text-neutral-700 hover:text-violet-600"
+            >
               <IconHome />
               Home
-            </a>
+            </Link>
           </li>
           {linkLevel1 && textLevel1 && (
             <li>
               <div className="flex items-center">
                 <IconCaretRight />
-                <a
+                <Link
                   href={linkLevel1}
                   className="ms-1 text-sm font-medium text-neutral-700 hover:text-violet-600 md:ms-2"
                 >
                   {textLevel1}
-                </a>
+                </Link>
               </div>
             </li>
           )}
@@ -29,12 +33,12 @@ const BreadCrumbs = ({ linkLevel1, textLevel1, linkLevel2, textLevel2 }: BreadCr
             <li>
               <div className="flex items-center">
                 <IconCaretRight />
-                <a
+                <Link
                   href={linkLevel2}
                   className="ms-1 text-sm font-medium text-neutral-700 hover:text-violet-600 md:ms-2"
                 >
                   {textLevel2}
-                </a>
+                </Link>
               </div>
             </li>
           )}
