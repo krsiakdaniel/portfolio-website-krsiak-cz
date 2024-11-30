3152d729f59a016934199e4a0f3b148940485626
Krsiak Daniel <krsiak.daniel@gmail.com>
2024-11-30 02:33:49 +0100

    test: ✅ E2E - Kooperativa

---
+++ __tests__/playwright/work-experience/projectsWorkKooperativa.spec.ts
@@ -0,0 +1,38 @@
+import { Browser, BrowserContext, Page, chromium, expect, test } from '@playwright/test'
+
+let browser: Browser
+let context: BrowserContext
+let page: Page
+
+test.beforeAll(async () => {
+  browser = await chromium.launch()
+})
+
+test.afterAll(async () => {
+  await browser.close()
+})
+
+test.beforeEach(async () => {
+  context = await browser.newContext()
+  page = await context.newPage()
+  await page.goto('/')
+})
+
+test.afterEach(async () => {
+  await context.close()
+})
+
+test.describe('Project - Kooperativa', () => {
+  test('Links', async ({ page }) => {
+    await test.step('Go to page', async () => {
+      await page.goto('/work-experience/kooperativa')
+    })
+
+    await test.step('Check Website link', async () => {
+      const link1 = page.getByRole('link', { name: 'Website →', exact: true })
+      const href1 = await link1.getAttribute('href')
+      const expectedUrl1 = 'https://www.koop.cz/pojisteni/pojisteni-majetku'
+      expect(href1).toBe(expectedUrl1)
+    })
+  })
+})
