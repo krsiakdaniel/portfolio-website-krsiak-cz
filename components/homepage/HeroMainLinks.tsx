a4ee345c4bf385244a3a159c128f08c692a8efac
Krsiak Daniel <krsiak.daniel@gmail.com>
2024-12-11 12:38:05 +0100

    refactor(buttons): 👷 change order on homepage (#125)

--- components/homepage/HeroMainLinks.tsx
+++ components/homepage/HeroMainLinks.tsx
@@ -1,5 +1,6 @@
 import { TEXT } from '@/localization/texts_en'
 import { EXTERNAL_URL } from '@/utils/constants'
+import { DATA_TEST_IDS } from '@/utils/data-testid'
 import HeroLink from './HeroLink'
 
 const HeroMainLinks = () => {
@@ -9,19 +10,19 @@ const HeroMainLinks = () => {
         href={EXTERNAL_URL.linkedin}
         text="My LinkedIn"
         className="mb-2 bg-blue-700 text-white hover:bg-blue-800 focus:ring-blue-300 sm:mb-0 sm:mr-2"
-        testId="hero-link-linkedin"
-      />
-      <HeroLink
-        href={EXTERNAL_URL.github}
-        text="GitHub Code"
-        className="mb-2 bg-gray-700 text-white hover:bg-gray-800 focus:ring-gray-300 sm:mb-0 sm:mr-2"
-        testId="hero-link-github"
+        testId={DATA_TEST_IDS.heroLinkLinkedIn}
       />
       <HeroLink
         href={EXTERNAL_URL.resume.resumeViewPDF}
         text={TEXT.resumePDF}
-        className="bg-red-700 text-white hover:bg-red-800 focus:ring-red-300 sm:mb-0"
-        testId="hero-link-resume"
+        className="mb-2 bg-red-700 text-white hover:bg-red-800 focus:ring-red-300 sm:mb-0 sm:mr-2"
+        testId={DATA_TEST_IDS.heroLinkResume}
+      />
+      <HeroLink
+        href={EXTERNAL_URL.github}
+        text="GitHub Code"
+        className="bg-gray-700 text-white hover:bg-gray-800 focus:ring-gray-300 sm:mb-0"
+        testId={DATA_TEST_IDS.heroLinkGitHub}
       />
     </div>
   )
