6f129f30692029177f99d4b0d3e26d1026e35dea
Krsiak Daniel <krsiak.daniel@gmail.com>
2024-12-11 20:22:08 +0100

    fix wrong constant

--- components/header/MenuDesktop.tsx
+++ components/header/MenuDesktop.tsx
@@ -6,13 +6,13 @@ const links = [
   { href: PAGES_URL.aboutMe, text: TEXT.aboutMe, testId: 'desktop-about-me-link', id: 'about-me' },
   { href: PAGES_URL.resume, text: TEXT.resume, testId: 'desktop-resume-link', id: 'resume' },
   {
-    href: PAGES_URL.work.main,
+    href: PAGES_URL.work.mainUrl,
     text: TEXT.workExperience,
     testId: 'desktop-work-experience-link',
     id: 'work-experience',
   },
   {
-    href: PAGES_URL.personal.main,
+    href: PAGES_URL.personal.mainUrl,
     text: TEXT.personalProjects,
     testId: 'desktop-personal-projects-link',
     id: 'personal-projects',
--- components/header/MenuMobile.tsx
+++ components/header/MenuMobile.tsx
@@ -6,13 +6,13 @@ const links = [
   { href: PAGES_URL.aboutMe, text: TEXT.aboutMe, testId: 'mobile-about-me-link', id: 'about-me' },
   { href: PAGES_URL.resume, text: TEXT.resume, testId: 'mobile-resume-link', id: 'resume' },
   {
-    href: PAGES_URL.work.main,
+    href: PAGES_URL.work.mainUrl,
     text: TEXT.workExperience,
     testId: 'mobile-work-experience-link',
     id: 'work-experience',
   },
   {
-    href: PAGES_URL.personal.main,
+    href: PAGES_URL.personal.mainUrl,
     text: TEXT.personalProjects,
     testId: 'mobile-personal-projects-link',
     id: 'personal-projects',
