03fdc6653d7d5cc174c48975af99793ac0ecb2aa
Krsiak Daniel <krsiak.daniel@gmail.com>
2024-12-14 22:05:50 +0100

    refactor(playwright): 👷 datatestid file readability (#179)

--- utils/dataTestIds.ts
+++ utils/dataTestIds.ts
@@ -1,131 +1,146 @@
-// TODO: refactor this file to be more readable
-const NEXT = 'next'
-const PREVIOUS = 'previous'
-const PAGE_NAVIGATION = 'page-navigation'
-const PERSONAL_PROJECTS = 'personal-projects'
-const WORK_EXPERIENCE = 'work-experience'
-const HERO_LINK = 'hero-link'
+const NAVIGATION = {
+  NEXT: 'page-navigation-next',
+  PREVIOUS: 'page-navigation-previous',
+}
 
-const LINKEDIN = 'linkedin'
-const RESUME = 'resume'
-const GITHUB = 'github'
-const HOMEPAGE = 'homepage'
-const ABOUT_ME = 'about-me'
-const KRSIAK = 'krsiak'
-const CRYPTOMANIA = 'cryptomania'
-const KOMERCNI_BANKA = 'komercni-banka'
-const SMARTSUPP_WEB = 'smartsupp-web'
-const SMARTSUPP_HELP = 'smartsupp-help'
-const MORAVIA = 'moravia'
-const GROUPON = 'groupon'
-const KOOPERATIVA = 'kooperativa'
+const SECTIONS = {
+  PERSONAL_PROJECTS: 'personal-projects',
+  WORK_EXPERIENCE: 'work-experience',
+  HERO_LINK: 'hero-link',
+}
+
+const LINKS = {
+  LINKEDIN: 'linkedin',
+  RESUME: 'resume',
+  GITHUB: 'github',
+  HOMEPAGE: 'homepage',
+  ABOUT_ME: 'about-me',
+  KRSIAK: 'krsiak',
+  CRYPTOMANIA: 'cryptomania',
+  KOMERCNI_BANKA: 'komercni-banka',
+  SMARTSUPP_WEB: 'smartsupp-web',
+  SMARTSUPP_HELP: 'smartsupp-help',
+  MORAVIA: 'moravia',
+  GROUPON: 'groupon',
+  KOOPERATIVA: 'kooperativa',
+}
+
+const FOOTER = {
+  scrollToTopButton: 'scroll-to-top-button',
+  copyright: 'footer-copyright',
+  link: 'footer-link',
+  statusPage: 'footer-link-status-page',
+}
+
+const MENU = {
+  mobile: {
+    component: 'menu-mobile',
+    links: {
+      aboutMe: 'mobile-about-me-link',
+      resume: 'mobile-resume-link',
+      workExperience: 'mobile-work-experience-link',
+      personalProjects: 'mobile-personal-projects-link',
+    },
+  },
+  desktop: {
+    component: 'menu-desktop',
+    links: {
+      aboutMe: 'desktop-about-me-link',
+      resume: 'desktop-resume-link',
+      workExperience: 'desktop-work-experience-link',
+      personalProjects: 'desktop-personal-projects-link',
+    },
+  },
+  toggle: {
+    component: 'menu-toggle',
+  },
+}
+
+const HERO = {
+  heading: 'hero-heading',
+  linkLinkedIn: `${SECTIONS.HERO_LINK}-${LINKS.LINKEDIN}`,
+  linkResume: `${SECTIONS.HERO_LINK}-${LINKS.RESUME}`,
+  linkGitHub: `${SECTIONS.HERO_LINK}-${LINKS.GITHUB}`,
+  paragraph: 'hero-paragraph',
+}
+
+const PAGE = {
+  home: {
+    previous: '',
+    next: `${NAVIGATION.NEXT}-${LINKS.ABOUT_ME}`,
+  },
+  aboutMe: {
+    previous: `${NAVIGATION.PREVIOUS}-${LINKS.HOMEPAGE}`,
+    next: `${NAVIGATION.NEXT}-${LINKS.RESUME}`,
+    introduction: 'about-me-introduction',
+  },
+  resume: {
+    previous: `${NAVIGATION.PREVIOUS}-${LINKS.ABOUT_ME}`,
+    next: `${NAVIGATION.NEXT}-${SECTIONS.WORK_EXPERIENCE}`,
+    introduction: 'resume-introduction',
+  },
+}
+
+const PROJECTS = {
+  personal: {
+    overview: {
+      previous: `${NAVIGATION.PREVIOUS}-${SECTIONS.WORK_EXPERIENCE}`,
+      next: '',
+    },
+    cryptoMania: {
+      previous: `${NAVIGATION.PREVIOUS}-${LINKS.KRSIAK}`,
+      next: '',
+    },
+    krsiak: {
+      previous: `${NAVIGATION.PREVIOUS}-${SECTIONS.PERSONAL_PROJECTS}`,
+      next: `${NAVIGATION.NEXT}-${LINKS.CRYPTOMANIA}`,
+    },
+  },
+  work: {
+    overview: {
+      previous: `${NAVIGATION.PREVIOUS}-${LINKS.RESUME}`,
+      next: `${NAVIGATION.NEXT}-${SECTIONS.PERSONAL_PROJECTS}`,
+    },
+    kooperativa: {
+      previous: `${NAVIGATION.PREVIOUS}-${LINKS.KOMERCNI_BANKA}`,
+      next: `${NAVIGATION.NEXT}-${LINKS.SMARTSUPP_WEB}`,
+    },
+    groupon: {
+      previous: `${NAVIGATION.PREVIOUS}-${LINKS.SMARTSUPP_HELP}`,
+      next: `${NAVIGATION.NEXT}-${LINKS.MORAVIA}`,
+    },
+    smartsupp: {
+      web: {
+        previous: `${NAVIGATION.PREVIOUS}-${LINKS.KOOPERATIVA}`,
+        next: `${NAVIGATION.NEXT}-${LINKS.SMARTSUPP_HELP}`,
+      },
+      dashboard: {
+        previous: `${NAVIGATION.PREVIOUS}-${SECTIONS.WORK_EXPERIENCE}`,
+        next: `${NAVIGATION.NEXT}-${LINKS.KOMERCNI_BANKA}`,
+      },
+      help: {
+        previous: `${NAVIGATION.PREVIOUS}-${LINKS.SMARTSUPP_WEB}`,
+        next: `${NAVIGATION.NEXT}-${LINKS.GROUPON}`,
+      },
+    },
+    komercniBanka: {
+      previous: `${NAVIGATION.PREVIOUS}-${LINKS.GROUPON}`,
+      next: `${NAVIGATION.NEXT}-${LINKS.KOOPERATIVA}`,
+    },
+    moravia: {
+      previous: `${NAVIGATION.PREVIOUS}-${LINKS.SMARTSUPP_HELP}`,
+      next: `${NAVIGATION.NEXT}-${LINKS.GROUPON}`,
+    },
+  },
+}
 
 export const DATA_TEST_IDS = {
   breadcrumbs: 'breadcrumbs',
   gallery: 'gallery',
   header: 'header-logo',
-  footer: {
-    scrollToTopButton: 'scroll-to-top-button',
-    copyright: 'footer-copyright',
-    link: 'footer-link',
-    statusPage: 'footer-link-status-page',
-  },
-  menu: {
-    mobile: {
-      component: 'menu-mobile',
-      links: {
-        aboutMe: 'mobile-about-me-link',
-        resume: 'mobile-resume-link',
-        workExperience: 'mobile-work-experience-link',
-        personalProjects: 'mobile-personal-projects-link',
-      },
-    },
-    desktop: {
-      component: 'menu-desktop',
-      links: {
-        aboutMe: 'desktop-about-me-link',
-        resume: 'desktop-resume-link',
-        workExperience: 'desktop-work-experience-link',
-        personalProjects: 'desktop-personal-projects-link',
-      },
-    },
-    toggle: {
-      component: 'menu-toggle',
-    },
-  },
-  hero: {
-    heading: 'hero-heading',
-    linkLinkedIn: `${HERO_LINK}-${LINKEDIN}`,
-    linkResume: `${HERO_LINK}-${RESUME}`,
-    linkGitHub: `${HERO_LINK}-${GITHUB}`,
-    paragraph: 'hero-paragraph',
-  },
-  page: {
-    home: {
-      previous: '',
-      next: `${PAGE_NAVIGATION}-${NEXT}-${ABOUT_ME}`,
-    },
-    aboutMe: {
-      previous: `${PAGE_NAVIGATION}-${PREVIOUS}-${HOMEPAGE}`,
-      next: `${PAGE_NAVIGATION}-${NEXT}-${RESUME}`,
-      introduction: 'about-me-introduction',
-    },
-    resume: {
-      previous: `${PAGE_NAVIGATION}-${PREVIOUS}-${ABOUT_ME}`,
-      next: `${PAGE_NAVIGATION}-${NEXT}-${WORK_EXPERIENCE}`,
-      introduction: 'resume-introduction',
-    },
-  },
-  projects: {
-    personal: {
-      overview: {
-        previous: `${PAGE_NAVIGATION}-${PREVIOUS}-${WORK_EXPERIENCE}`,
-        next: '',
-      },
-      cryptoMania: {
-        previous: `${PAGE_NAVIGATION}-${PREVIOUS}-${KRSIAK}`,
-        next: '',
-      },
-      krsiak: {
-        previous: `${PAGE_NAVIGATION}-${PREVIOUS}-${PERSONAL_PROJECTS}`,
-        next: `${PAGE_NAVIGATION}-${NEXT}-${CRYPTOMANIA}`,
-      },
-    },
-    work: {
-      overview: {
-        previous: `${PAGE_NAVIGATION}-${PREVIOUS}-${RESUME}`,
-        next: `${PAGE_NAVIGATION}-${NEXT}-${PERSONAL_PROJECTS}`,
-      },
-      kooperativa: {
-        previous: `${PAGE_NAVIGATION}-${PREVIOUS}-${KOMERCNI_BANKA}`,
-        next: `${PAGE_NAVIGATION}-${NEXT}-${SMARTSUPP_WEB}`,
-      },
-      groupon: {
-        previous: `${PAGE_NAVIGATION}-${PREVIOUS}-${SMARTSUPP_HELP}`,
-        next: `${PAGE_NAVIGATION}-${NEXT}-${MORAVIA}`,
-      },
-      smartsupp: {
-        web: {
-          previous: `${PAGE_NAVIGATION}-${PREVIOUS}-${KOOPERATIVA}`,
-          next: `${PAGE_NAVIGATION}-${NEXT}-${SMARTSUPP_HELP}`,
-        },
-        dashboard: {
-          previous: `${PAGE_NAVIGATION}-${PREVIOUS}-${WORK_EXPERIENCE}`,
-          next: `${PAGE_NAVIGATION}-${NEXT}-${KOMERCNI_BANKA}`,
-        },
-        help: {
-          previous: `${PAGE_NAVIGATION}-${PREVIOUS}-${SMARTSUPP_WEB}`,
-          next: `${PAGE_NAVIGATION}-${NEXT}-${GROUPON}`,
-        },
-      },
-      komercniBanka: {
-        previous: `${PAGE_NAVIGATION}-${PREVIOUS}-${GROUPON}`,
-        next: `${PAGE_NAVIGATION}-${NEXT}-${KOOPERATIVA}`,
-      },
-      moravia: {
-        previous: `${PAGE_NAVIGATION}-${PREVIOUS}-${SMARTSUPP_HELP}`,
-        next: `${PAGE_NAVIGATION}-${NEXT}-${GROUPON}`,
-      },
-    },
-  },
+  footer: FOOTER,
+  menu: MENU,
+  hero: HERO,
+  page: PAGE,
+  projects: PROJECTS,
 }
