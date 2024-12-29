6b673dc7dc34796e77559d26f8ce35e9e0f28988
Krsiak Daniel <krsiak.daniel@gmail.com>
2024-12-30 00:02:36 +0100

    refactor(manifest): 👷 improve the content
    - manifest
    - readme screenshots

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,6 @@ export const metadata: Metadata = {
   generator: 'Next.js',
   applicationName: 'React - Portfolio website',
   referrer: 'origin-when-cross-origin',
-  themeColor: '#ECF0F1',
   formatDetection: {
     email: false,
     address: false,
@@ -38,6 +37,17 @@ export default function RootLayout({
     <html lang="en">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#ECF0F1" />
+        <link rel="apple-touch-icon" sizes="57x57" href="/icons/png/icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/icons/png/icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/icons/png/icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/icons/png/icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/icons/png/icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/icons/png/icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/icons/png/icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/png/icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/png/icon-180x180.png" />
+        <link rel="apple-touch-icon" sizes="192x192" href="/icons/png/icon-192x192.png" />
       </Head>
       <body className={inter.className}>
         <Header />
