47564c17a0a5db3faa04b3e457550709a01addae
Krsiak Daniel <krsiak.daniel@gmail.com>
2024-12-31 01:22:03 +0100

    refactor(footer): 👷 contact links (#273)

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,16 +1,18 @@
-import FooterLeft from '@/components/footer/FooterLeft'
-import FooterRight from '@/components/footer/FooterRight'
+import FooterCopyright from '@/components/footer/FooterCopyright'
+import FooterStatusAndScroll from '@/components/footer/FooterStatusAndScroll'
 import PageContainer from '@/components/layout/PageContainer'
 
+import FooterMainLinks from '@/components/footer/FooterMainLinks'
 import { ID } from '@/lib/utils/constants/ids/elementIds'
 
 const Footer = (): JSX.Element => {
   return (
     <PageContainer id={ID.footer}>
       <footer className="border-t border-dashed bg-white">
-        <div className="mx-auto flex w-full flex-col items-center justify-between py-4 text-center text-neutral-600 md:flex-row">
-          <FooterLeft />
-          <FooterRight />
+        <div className="mx-auto flex w-full flex-col items-center py-8 text-center text-neutral-600">
+          <FooterMainLinks />
+          <FooterStatusAndScroll />
+          <FooterCopyright />
         </div>
       </footer>
     </PageContainer>
---
+++ components/footer/FooterCopyright.tsx
@@ -0,0 +1,21 @@
+import { DATA_TEST_IDS } from '@/__tests__/playwright/lib/utils/constants/ids/dataTestIds'
+import { PAGES_URL } from '@/lib/utils/constants/urls/pageUrls'
+
+import { getCurrentYear } from '@/lib/utils/helpers/getCurrentYear'
+import Link from 'next/link'
+
+export const FooterCopyright = (): JSX.Element => {
+  return (
+    <div className="mt-8">
+      <span className="ml-2" data-testid={DATA_TEST_IDS.footer.copyright}>
+        😼 😺 Copyright ©&nbsp;{getCurrentYear()}
+        {' | '}
+        <Link href={PAGES_URL.home} className="underline hover:no-underline" data-testid={DATA_TEST_IDS.footer.link}>
+          krsiak.cz
+        </Link>
+      </span>
+    </div>
+  )
+}
+
+export default FooterCopyright
