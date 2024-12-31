eeacb90f00e88b3c1f41b8170b9fc126d1534b33
Krsiak Daniel <krsiak.daniel@gmail.com>
2024-12-31 11:40:37 +0100

    fix(heading): 🐛 wrong H2 correct H3 + texts

--- app/testimonials/page.tsx
+++ app/testimonials/page.tsx
@@ -31,7 +31,7 @@ const About = (): JSX.Element => {
           </p>
 
           <p className="text-lg text-neutral-600">
-            Below, you'll find insightful feedback from colleagues and collaborators who have experienced my
+            Below, you&apos;ll find insightful feedback from colleagues and collaborators who have experienced my
             contributions firsthand.
           </p>
         </div>
@@ -45,15 +45,15 @@ const About = (): JSX.Element => {
                 <strong>💼 Position:</strong> QA Tester and Content Manager <strong>~ Smartlook</strong>
               </li>
               <li>
-                <strong>🤝 Work Relationship:</strong> Daniel was senior to Alexandre but didn't manage him directly.
+                <strong>🤝 Work Relationship:</strong> Daniel was senior to Alexandre but didn&apos;t manage him directly.
               </li>
               <li>
                 <strong>💬 Testimonial:</strong>
-                "His vast work experience in diverse environments and positions has endowed him with a unique set of
+                &quot;His vast work experience in diverse environments and positions has endowed him with a unique set of
                 both soft and hard skills, making him a team member that brings added value to every organization. On a
                 personal note, Daniel helped me a great deal when I first started at Smartsupp. Thanks to him, I have
                 managed to gain proficiency in a wide set of tools and working techniques. To this day, I still rely on
-                Daniel as a go-to person for advice on specific topics related to my work and personal side projects."
+                Daniel as a go-to person for advice on specific topics related to my work and personal side projects.&quot;
               </li>
             </ul>
           </p>
@@ -69,8 +69,8 @@ const About = (): JSX.Element => {
               </li>
               <li>
                 <strong>💬 Testimonial:</strong>
-                "I can recommend his knowledge and perspective about the IT globally and his behavior as a person are
-                excellent for team work."
+                &quot;I can recommend his knowledge and perspective about the IT globally and his behavior as a person are
+                excellent for team work.&quot;
               </li>
             </ul>
           </p>
@@ -82,13 +82,13 @@ const About = (): JSX.Element => {
                 <strong>💼 Position:</strong> Senior Localization Group Manager <strong>~ Moravia IT (RWS)</strong>
               </li>
               <li>
-                <strong>🤝 Work Relationship:</strong> Vojtech was senior to Daniel but didn't manage him directly.
+                <strong>🤝 Work Relationship:</strong> Vojtech was senior to Daniel but didn&apos;t manage him directly.
               </li>
               <li>
                 <strong>💬 Testimonial:</strong>
-                "Daniel has helped the team with his engineering skills focusing on automating the routine manual bug
+                &quot;Daniel has helped the team with his engineering skills focusing on automating the routine manual bug
                 management tasks, working in the challenging role of bugfixing engineer bridging the linguistic managers
-                and the production teams."
+                and the production teams.&quot;
               </li>
             </ul>
           </p>
@@ -104,8 +104,8 @@ const About = (): JSX.Element => {
                 <strong>🤝 Work Relationship:</strong> Inka worked with Daniel but on different teams.
               </li>
               <li>
-                <strong>💬 Testimonial:</strong> "Daniel is strong in creating all kinds of automation utilities (term
-                processing, all kinds of tracking, etc.). He has gained a lot of experience in bug processing."
+                <strong>💬 Testimonial:</strong> &quot;Daniel is strong in creating all kinds of automation utilities (term
+                processing, all kinds of tracking, etc.). He has gained a lot of experience in bug processing.&quot;
               </li>
             </ul>
           </p>
@@ -120,9 +120,9 @@ const About = (): JSX.Element => {
                 <strong>🤝 Work Relationship:</strong> Daniel worked with Daniel but on different teams.
               </li>
               <li>
-                <strong>💬 Testimonial:</strong> "I met Daniel Krsiak while working on different teams, he was my
+                <strong>💬 Testimonial:</strong> &quot;I met Daniel Krsiak while working on different teams, he was my
                 desk-mate. I found him passionate about new technologies, eager to learn all the time, communicative and
-                willing to share his knowledge."
+                willing to share his knowledge.&quot;
               </li>
             </ul>
           </p>
