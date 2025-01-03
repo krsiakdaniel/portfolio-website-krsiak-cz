89578d0690b077f86939ce5544674aa4c6770901
Krsiak Daniel <krsiak.daniel@gmail.com>
2025-01-03 11:46:09 +0100

    hero - SocialLink

--- components/pages/home/hero/HeroSocialLinks.tsx
+++ components/pages/home/hero/HeroSocialLinks.tsx
@@ -5,27 +5,29 @@ import { TEXT } from '@/localization/english'
 import { DATA_TEST_IDS } from '@/__tests__/playwright/lib/utils/constants/ids/dataTestIds'
 import { EXTERNAL_URL } from '@/lib/utils/constants/urls/externalUrls'
 
+import { LinkColors } from '@/lib/utils/interfaces/enums'
+
 const HeroSocialLinks = (): JSX.Element => {
   return (
     <div className="mt-10 flex flex-col items-center justify-center sm:flex-row">
       <SocialLink
         href={EXTERNAL_URL.linkedin}
         text={TEXT.linkedIn}
-        linkColor="blue"
+        linkColor={LinkColors.Blue}
         className="sm:mr-4"
         dataTestId={DATA_TEST_IDS.hero.linkLinkedIn}
       />
       <SocialLink
         href={EXTERNAL_URL.resume.resumeViewPDF}
         text={TEXT.resumePDF}
-        linkColor="red"
+        linkColor={LinkColors.Red}
         className="mt-4 sm:mr-4 sm:mt-0"
         dataTestId={DATA_TEST_IDS.hero.linkResume}
       />
       <SocialLink
         href={EXTERNAL_URL.github}
         text={TEXT.gitHub}
-        linkColor="gray"
+        linkColor={LinkColors.Gray}
         className="mt-4 sm:mt-0"
         dataTestId={DATA_TEST_IDS.hero.linkGitHub}
       />
