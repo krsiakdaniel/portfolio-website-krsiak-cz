e587f4dce2ee8d667e29711a2c943069e9dd0c04
Krsiak Daniel <krsiak.daniel@gmail.com>
2025-01-05 19:38:17 +0100

    test(jest): ✅ project icons - overview page (#317)

---
+++ __tests__/jest/skills/iconsSkillsProjectsOverviewPersonal.test.ts
@@ -0,0 +1,28 @@
+import {
+  iconsSkillsProjectsPersonalCryptomania,
+  iconsSkillsProjectsPersonalKrsiak,
+} from '@/lib/data/pages/projects/personal/projects-overview/skillsProjectsPersonal'
+
+import { getSkillsIcons } from '@/lib/utils/helpers/getSkillsIcons'
+
+describe('iconsSkillsProjectsPersonal', () => {
+  it('should return the correct icons for Krsiak project', () => {
+    const expectedIcons = getSkillsIcons([
+      'javascript',
+      'typescript',
+      'react',
+      'next',
+      'tailwind',
+      'jest',
+      'playwright',
+      'githubActions',
+      'netlify',
+    ])
+    expect(iconsSkillsProjectsPersonalKrsiak).toEqual(expectedIcons)
+  })
+
+  it('should return the correct icons for Cryptomania project', () => {
+    const expectedIcons = getSkillsIcons(['javascript', 'typescript', 'react', 'chakra', 'netlify'])
+    expect(iconsSkillsProjectsPersonalCryptomania).toEqual(expectedIcons)
+  })
+})
