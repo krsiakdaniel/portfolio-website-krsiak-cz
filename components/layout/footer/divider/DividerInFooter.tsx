e963fe71ce412b2889550abbce2b657ab9f38bd7
Krsiak Daniel <krsiak.daniel@gmail.com>
2025-01-06 02:21:03 +0100

    easter egg - comment

--- components/layout/footer/divider/DividerInFooter.tsx
+++ components/layout/footer/divider/DividerInFooter.tsx
@@ -7,11 +7,23 @@ const sharedCSS = 'mx-2 h-1 w-full flex-1'
 const sunRiseToNoon = <div className={`bg-gradient-to-r from-yellow-400 via-sky-300 to-green-500 ${sharedCSS}`} />
 const noonToSunSet = <div className={`to-gray-800" bg-gradient-to-r from-green-500 via-blue-700 ${sharedCSS}`} />
 
-// Klub českých turistů
-// https://www.kct.cz/
-
-// Stezky z lásky
-// https://www.facebook.com/groups/954188525874944
+/**
+ * @description
+ *
+ * Klub českých turistů
+ * https://www.kct.cz/
+ *
+ * Stezky z lásky
+ * https://www.facebook.com/groups/954188525874944
+ *
+ * I am a scout, and scout leader, deeply connected with nature.
+ * I spend my days trailblazing, painting hiking posts, and embracing the wilderness.
+ * My love for nature and the outdoors has been a lifelong journey.
+ * Whether it's navigating new paths or appreciating the beauty of the natural world, every step I take fuels my passion for the great outdoors.
+ *
+ * @joke Why don't mountains get cold in the winter? Because they wear snowcaps!
+ *
+ */
 
 const DividerWithGradient = () => {
   return (
