2e3ca7505ba6cae48805af8d01afe02cd8176eb8
Krsiak Daniel <krsiak.daniel@gmail.com>
2025-01-16 00:31:04 +0100

    refactor: 👷 data order

--- lib/data/pages/home/expertise/problemSolving.ts
+++ lib/data/pages/home/expertise/problemSolving.ts
@@ -1,9 +1,9 @@
 import { SkillsInfo } from '@/lib/utils/interfaces/interfaces'
 
 export const problemSolving: SkillsInfo[] = [
-  { id: 0, text: 'Quick to adapt to changes' },
-  { id: 1, text: 'Creativity and pragmatic' },
   { id: 2, text: 'Ability to think outside the box' },
   { id: 3, text: 'Enjoy brainstorming sessions' },
+  { id: 0, text: 'Quick to adapt to changes' },
   { id: 4, text: 'Independent and curious' },
+  { id: 1, text: 'Creative and pragmatic' },
 ]
