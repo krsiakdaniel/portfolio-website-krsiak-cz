d400d83fc0b6897ba93a2d690d1e7dc962a31a14
Krsiak Daniel <krsiak.daniel@gmail.com>
2025-01-17 03:15:35 +0100

    fix: 🐛 import

--- components/shared/SocialLink.tsx
+++ components/shared/SocialLink.tsx
@@ -4,7 +4,7 @@ import IconExternalLink from '@/components/icons/IconExternalLink'
 
 import { LinkColors } from '@/lib/utils/typeDefinitions/enums'
 
-import { SocialLinkProps } from '@/lib/utils/typeDefinitions/props/props'
+import { SocialLinkProps } from '@/lib/utils/typeDefinitions/props/shared/social-link'
 
 const SocialLink: FC<SocialLinkProps> = ({
   href,
