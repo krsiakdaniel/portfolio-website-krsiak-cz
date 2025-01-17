8e9b9e6fb72babb56e6346661540e089f1f27e19
Krsiak Daniel <krsiak.daniel@gmail.com>
2025-01-18 00:32:06 +0100

    refactor(text): 👷 minor update (#383)

--- lib/data/pages/resume/career-path/careerFrontEnd.ts
+++ lib/data/pages/resume/career-path/careerFrontEnd.ts
@@ -2,6 +2,6 @@ import { SkillsInfo } from '@/lib/utils/typeDefinitions/interfaces'
 
 export const careerFrontEnd: SkillsInfo[] = [
   { id: 0, text: '💻 Smartsupp - Website · 1 year' },
-  { id: 1, text: '💼 Freelance - Company Websites · 5 years' },
+  { id: 1, text: '💼 Freelance - Websites · 5 years' },
   { id: 2, text: '👨🏻‍💻 Freelance - Web Coder · 1 year' },
 ]
