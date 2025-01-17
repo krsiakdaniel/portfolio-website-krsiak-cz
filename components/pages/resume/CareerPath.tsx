2a374be5f7ea1ccd4b1e14d62e34a3fdc8dafddd
Krsiak Daniel <krsiak.daniel@gmail.com>
2025-01-17 23:05:44 +0100

    refactor: 👷 career path

--- components/pages/resume/CareerPath.tsx
+++ components/pages/resume/CareerPath.tsx
@@ -1,8 +1,8 @@
 import { FC } from 'react'
 
 import ExpertiseSection from '@/components/pages/home/expertise/ExpertiseSection'
-
 import Paragraph from '@/components/shared/Paragraph'
+
 import { careerAutomation } from '@/lib/data/pages/resume/career-path/careerAutomation'
 import { careerFrontEnd } from '@/lib/data/pages/resume/career-path/careerFrontEnd'
 import { careerReact } from '@/lib/data/pages/resume/career-path/careerReact'
@@ -10,26 +10,24 @@ import { careerReact } from '@/lib/data/pages/resume/career-path/careerReact'
 const CareerPath: FC = (): JSX.Element => {
   return (
     <>
+      <div className="mt-16 border-b pb-2 text-2xl font-bold uppercase">My Career Path</div>
+
       <div>
-        <div className="mt-16 border-b pb-2 text-2xl font-bold uppercase">My Career Path</div>
+        <Paragraph size="text-sm">
+          <strong>My journey:</strong> 🏢 IBM · Mainframes ← ✈️ Airport · Ticket Agent ← 🍗 KFC ← 📊 High School ·
+          Accountant
+        </Paragraph>
+      </div>
 
-        <div>
-          <Paragraph size="text-sm">
-            <strong>My journey:</strong> 🏢 IBM · Mainframes ← ✈️ Airport · Ticket Agent ← 🍗 KFC ← 📊 High School ·
-            Accountant
-          </Paragraph>
+      <div className="mt-8 flex flex-wrap gap-20">
+        <div className="min-w-[300px] flex-1">
+          <ExpertiseSection heading="⚛️ React" listItems={careerReact} />
         </div>
-
-        <div className="mt-5 flex flex-wrap gap-20">
-          <div className="min-w-[300px] flex-1">
-            <ExpertiseSection heading="⚛️ React" listItems={careerReact} />
-          </div>
-          <div className="min-w-[300px] flex-1">
-            <ExpertiseSection heading="🖥️ Front End" listItems={careerFrontEnd} />
-          </div>
-          <div className="min-w-[300px] flex-1">
-            <ExpertiseSection heading="⚙️ Testing" listItems={careerAutomation} />
-          </div>
+        <div className="min-w-[300px] flex-1">
+          <ExpertiseSection heading="🖥️ Front End" listItems={careerFrontEnd} />
+        </div>
+        <div className="min-w-[300px] flex-1">
+          <ExpertiseSection heading="⚙️ Testing" listItems={careerAutomation} />
         </div>
       </div>
     </>
