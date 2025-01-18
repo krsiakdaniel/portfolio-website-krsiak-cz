3e109f57b33e3f18bb8c3c099c4de0bcb7adc696
Krsiak Daniel <krsiak.daniel@gmail.com>
2025-01-18 02:39:10 +0100

    refactor(resume): 👷open to work

--- components/shared/Photo.tsx
+++ components/shared/Photo.tsx
@@ -5,20 +5,24 @@ import Paragraph from './Paragraph'
 
 import { TEXT } from '@/localization/english'
 
+import openToWork from '@/public/images/png/open-to-work.png'
 import krsiak from '@/public/images/webp/photo/krsiak-daniel-296x296.webp'
 
 const Photo: FC = (): JSX.Element => {
   return (
     <div className="flex flex-col">
-      <Image
-        src={krsiak}
-        alt={TEXT.nameDanielKrsiak}
-        className="rounded-lg bg-violet-100 p-1 ring-1 ring-violet-300 lg:mt-0"
-        placeholder="blur"
-        loading="eager"
-      />
+      <div className="relative rounded-full bg-black">
+        <Image
+          src={krsiak}
+          alt={TEXT.nameDanielKrsiak}
+          className="rounded-full bg-violet-100 p-1 ring-1 ring-violet-300 lg:mt-0"
+          placeholder="blur"
+          loading="eager"
+        />
+        <Image src={openToWork} alt="Open To Work" className="absolute left-0 top-0 h-full w-full" />
+      </div>
       <div>
-        <Paragraph marginTop="mt-2" size="text-sm" textColor="text-neutral-500">
+        <Paragraph marginTop="mt-2" size="text-sm" textColor="text-neutral-500" customCss="text-center">
           {TEXT.nameDanielKrsiak}
         </Paragraph>
       </div>
