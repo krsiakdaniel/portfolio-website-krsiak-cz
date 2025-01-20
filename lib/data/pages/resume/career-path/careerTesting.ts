2d2e0845bf32f67fd654927b196286800dd05596
Krsiak Daniel <krsiak.daniel@gmail.com>
2025-01-20 19:52:20 +0100

    refactor(resume): 👷 new url cache busting (#402)

--- lib/data/pages/resume/career-path/careerTesting.ts
+++ lib/data/pages/resume/career-path/careerTesting.ts
@@ -1,6 +1,6 @@
 import { SkillsInfo } from '@/lib/utils/typeDefinitions/interfaces'
 
-export const careerAutomation: SkillsInfo[] = [
+export const careerTesting: SkillsInfo[] = [
   { id: 0, text: '🤖 QA Automation - Team Lead', years: '1 year' },
   { id: 1, text: '🇨🇿 Localization - Manager', years: '2 years' },
   { id: 2, text: '🈺 Localization - Tester', years: '3 years' },
