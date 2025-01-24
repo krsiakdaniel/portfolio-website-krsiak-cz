cdd800a05134345b90366299036ad1e390ee8e9b
Krsiak Daniel <krsiak.daniel@gmail.com>
2025-01-24 23:21:16 +0100

    refactor(footer): 👷remove social links

--- components/layout/footer/Footer.tsx
+++ components/layout/footer/Footer.tsx
@@ -1,8 +1,10 @@
 import { FC } from 'react'
 
 import DividerInFooter from '@/components/layout/footer/divider/DividerInFooter'
-import FooterContent from '@/components/layout/footer/FooterContent'
+import FooterCopyright from '@/components/layout/footer/FooterCopyright'
+import FooterStatusAndScroll from '@/components/layout/footer/FooterStatusAndScroll'
 import PageContainer from '@/components/layout/PageContainer'
+import ScanMyContactQR from '@/components/shared/ScanMyContactQR'
 
 import { ID } from '@/lib/utils/constants/ids/elementIds'
 
@@ -11,7 +13,12 @@ const Footer: FC = (): JSX.Element => {
     <PageContainer id={ID.footer}>
       <footer>
         <DividerInFooter />
-        <FooterContent />
+
+        <div className="mx-auto mt-20 flex w-full flex-col items-center justify-center pb-16">
+          <ScanMyContactQR showDivider={true} />
+          <FooterStatusAndScroll />
+          <FooterCopyright />
+        </div>
       </footer>
     </PageContainer>
   )
