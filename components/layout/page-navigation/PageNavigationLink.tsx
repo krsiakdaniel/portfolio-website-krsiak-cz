d9631b561bf5e317d7e5ee6137330d085c45d09c
Krsiak Daniel <krsiak.daniel@gmail.com>
2025-01-25 22:10:18 +0100

    refactor: 👷 PageNavigationLink rel = next / prev

--- components/layout/page-navigation/PageNavigationLink.tsx
+++ components/layout/page-navigation/PageNavigationLink.tsx
@@ -17,6 +17,7 @@ const PageNavigationLink: FC<PageNavigationLinkProps> = ({ href, text, arrowDire
   return (
     <a
       href={href}
+      rel={hasArrowLeft ? 'prev' : 'next'}
       className={`flex w-full items-center space-x-2 rounded-lg bg-violet-50 p-4 font-bold text-violet-500 ${justifyArrowCSS} ${hoverAndFocusCSS} select-none lg:w-1/2`}
       data-testid={dataTestId}
     >
