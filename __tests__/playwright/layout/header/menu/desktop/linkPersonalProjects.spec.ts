aa1175cf9167d82347e1e7ec0638d9fb4cc28766
Krsiak Daniel <krsiak.daniel@gmail.com>
2025-01-30 01:02:15 +0100

    performance(gtm): 🚀 tags - smartlook + google analytics (#471)
    * performance(gtm): 🚀 tags - smartlook + google analytics
    
    * fix: 🐛 install - @next/third-parties/google
    
    * test(e2e) 🎭 update for failing build

--- __tests__/playwright/layout/header/menu/desktop/linkPersonalProjects.spec.ts
+++ __tests__/playwright/layout/header/menu/desktop/linkPersonalProjects.spec.ts
@@ -16,7 +16,7 @@ test.describe('Header - Desktop Menu - Personal Projects Link', () => {
 
     await test.step('Check if the link redirects to the personal projects page when clicked', async () => {
       await page.click(getDataTestId(DATA_TEST_IDS.menu.desktop.links.personalProjects))
-      await page.waitForLoadState('networkidle')
+      await page.waitForURL('http://localhost:3000/personal-projects')
       expect(page.url()).toBe('http://localhost:3000/personal-projects')
     })
 
