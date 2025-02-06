8042969412a1f709d763f01fcb74e873bff5f346
Krsiak Daniel <krsiak.daniel@gmail.com>
2025-02-06 04:06:47 +0100

    refactor(analytics): 👷 remove gtm insert scripts

---
+++ components/analytics/Smartlook.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import Script from 'next/script'
+import { FC } from 'react'
+
+import { ANALYTICS_WARNING } from '@/localization/english'
+
+const Smartlook: FC = () => {
+  if (!process.env['NEXT_PUBLIC_SMARTLOOK_ID']) {
+    console.warn(ANALYTICS_WARNING.smartlook)
+    return null
+  }
+
+  return (
+    <Script id="smartlook" strategy="afterInteractive">
+      {`
+        window.smartlook||(function(d) {
+          var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+          var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+          c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
+        })(document);
+        smartlook('init', '${process.env['NEXT_PUBLIC_SMARTLOOK_ID']}', { region: 'eu' });
+      `}
+    </Script>
+  )
+}
+
+export default Smartlook
