beb4c03aa4b766171a8bd2bb82f2030b39d5e90a
Krsiak Daniel <krsiak.daniel@gmail.com>
2025-02-10 08:11:15 +0100

    refactor(misc): 👷 code, years, who i am

--- components/shared/HeadingSection.tsx
+++ components/shared/HeadingSection.tsx
@@ -19,13 +19,13 @@ const HeadingSection: FC<HeadingSectionProps> = ({
       data-testid={dataTestId}
       customCss={`mt-20 border-b pb-2 font-medium uppercase flex items-center ${CSS_GLOBAL_CLASSES.HEADING_SECTION}`}
     >
-      <span className="flex items-center">
-        {text}
+      <span className="flex items-start space-x-2 md:items-center">
         {icon && (
-          <span role="img" className="ml-2 inline-flex text-3xl">
+          <span role="img" className="inline-flex text-4xl">
             {icon}
           </span>
         )}
+        <span>{text}</span>
       </span>
     </Heading2>
   )
--- lib/data/pages/who-i-am/travelItems.ts
+++ lib/data/pages/who-i-am/travelItems.ts
@@ -5,25 +5,21 @@ import { ICON_EMOJI, WHO_I_AM } from '@/localization/english'
 export const travelItems: TravelItem[] = [
   {
     id: WHO_I_AM.idCZ,
-    icon: ICON_EMOJI.evergreenTree,
     flag: ICON_EMOJI.flagCzechRepublic,
     text: WHO_I_AM.textCzechRepublic,
   },
   {
     id: WHO_I_AM.idSK,
-    icon: ICON_EMOJI.mountain,
     flag: ICON_EMOJI.flagSlovakia,
     text: WHO_I_AM.textSlovakia,
   },
   {
     id: WHO_I_AM.idFI,
-    icon: ICON_EMOJI.ship,
     flag: ICON_EMOJI.flagAlandIslands,
     text: WHO_I_AM.textAlandIslands,
   },
   {
     id: WHO_I_AM.idKR,
-    icon: ICON_EMOJI.japaneseCastle,
     flag: ICON_EMOJI.flagSouthKorea,
     text: WHO_I_AM.textKorea,
   },
