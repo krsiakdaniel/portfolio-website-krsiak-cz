fbb5eb324ce6df72f1d12ada71b4da1425c522f1
Krsiak Daniel <krsiak.daniel@gmail.com>
2025-02-15 22:56:54 +0100

    refactor(resume): 👷 layout path and contact (#518)

--- components/pages/resume/CareerPath.tsx
+++ components/pages/resume/CareerPath.tsx
@@ -1,13 +1,12 @@
 import { FC } from 'react'
 
 import ExpertiseSection from '@/components/pages/home/expertise/ExpertiseSection'
-import CareerPathJobStep from '@/components/pages/resume/CareerPathJobStep'
 import HeadingSection from '@/components/shared/HeadingSection'
+import Highlight from '@/components/shared/Highlight'
 import Paragraph from '@/components/shared/Paragraph'
 
-import { ICON_EMOJI, TEXT } from '@/localization/english'
+import { CAREER_PATH, ICON_EMOJI, SHARED, TEXT } from '@/localization/english'
 
-import { careerPathSteps } from '@/lib/data/pages/resume/career-path/careerPathSteps'
 import { careerReactDev } from '@/lib/data/pages/resume/career-path/careerReactDev'
 import { careerTesting } from '@/lib/data/pages/resume/career-path/careerTesting'
 import { careerWebDev } from '@/lib/data/pages/resume/career-path/careerWebDev'
@@ -15,17 +14,16 @@ import { careerWebDev } from '@/lib/data/pages/resume/career-path/careerWebDev'
 const CareerPath: FC = (): JSX.Element => {
   return (
     <>
-      <HeadingSection text="Career Path" />
+      <HeadingSection text={CAREER_PATH.heading} />
 
-      <Paragraph size="text-sm" customCss="hidden xl:block">
-        {(careerPathSteps ?? []).map((step, index) => (
-          <CareerPathJobStep key={step.id} step={step} index={index} />
-        ))}
+      <Paragraph>
+        {CAREER_PATH.text1} <Highlight text={CAREER_PATH.text2} />
+        {SHARED.textDot}
       </Paragraph>
 
       <div className="mt-10 w-full">
-        <div className="grid w-full gap-6 lg:grid-cols-3">
-          <div className="grid w-full gap-6 md:grid-cols-2 lg:col-span-2">
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col gap-6">
             <ExpertiseSection
               icon={ICON_EMOJI.atomSymbol}
               heading={TEXT.reactDev}
@@ -37,13 +35,11 @@ const CareerPath: FC = (): JSX.Element => {
               listItems={careerWebDev}
             />
           </div>
-          <div className="w-full">
-            <ExpertiseSection
-              icon={ICON_EMOJI.cog}
-              heading={TEXT.testing}
-              listItems={careerTesting}
-            />
-          </div>
+          <ExpertiseSection
+            icon={ICON_EMOJI.cog}
+            heading={TEXT.testing}
+            listItems={careerTesting}
+          />
         </div>
       </div>
     </>
