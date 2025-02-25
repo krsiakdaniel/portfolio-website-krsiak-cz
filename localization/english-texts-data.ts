19414e44d5098e40b8fe85a2c9558dcad6ed748e
Krsiak Daniel <krsiak.daniel@gmail.com>
2025-02-25 22:54:17 +0100

    feature(testimonials): ✨coach - text from Marco

--- localization/english-texts-data.ts
+++ localization/english-texts-data.ts
@@ -62,14 +62,27 @@ export const TESTIMONIALS = {
       name: 'Shane Duquette',
       job: 'Founder',
       company: 'Bony\u00A0to\u00A0Beastly',
-      text: `Daniel has worked for\u00A0me\u00A0as\u00A0an\u00A0online fitness coach for\u00A0almost 10 years now, consistently and\u00A0with incredible attention to\u00A0detail. He's also a\u00A0pleasure to\u00A0work with and\u00A0popular with clients.`,
+      text: `Daniel has worked for\u00A0me\u00A0as\u00A0an\u00A0online fitness coach for\u00A0almost 10\u00A0years now, consistently and\u00A0with incredible attention to\u00A0detail.
+        He's also a\u00A0pleasure to\u00A0work with and\u00A0popular with clients.`,
+      linkText: 'Bony to Beastly',
+    },
+    marco: {
+      name: 'Marco Walker-Ng',
+      job: 'Founder',
+      company: 'Bony\u00A0to\u00A0Beastly',
+      text: `I've been working with Daniel for\u00A010\u00A0years managing an\u00A0online exercise forum.
+        We help people achieve their fitness goals and\u00A0the\u00A0forum is\u00A0a\u00A0place to\u00A0troubleshoot, find support and\u00A0be\u00A0a\u00A0part of\u00A0a\u00A0community.
+        Daniel has taken his life experiences and\u00A0used them to\u00A0become a\u00A0well-loved coach and\u00A0member of\u00A0our community.
+        He\u00A0is\u00A0very supportive, proactive and\u00A0has\u00A0helped countless members through hard times.
+        We are lucky to\u00A0have him.`,
       linkText: 'Bony to Beastly',
     },
     robert: {
       name: 'Robert Kesterson',
       job: 'Coach',
       company: 'Bony\u00A0to\u00A0Beastly',
-      text: `In the\u00A06 years I've worked with Daniel, he\u00A0has always been quick to\u00A0jump in\u00A0and\u00A0help to\u00A0solve every problem. His\u00A0generosity and\u00A0creativity are\u00A0inspirational to\u00A0everyone around him.”`,
+      text: `In the\u00A06 years I've worked with Daniel, he\u00A0has always been quick to\u00A0jump in\u00A0and\u00A0help to\u00A0solve every problem.
+        His\u00A0generosity and\u00A0creativity are\u00A0inspirational to\u00A0everyone around him.`,
     },
     steve: {
       name: 'Steve McManus',
