4802d6639e60c464ca7eda419be3c44f52c5d6bb
Krsiak Daniel <krsiak.daniel@gmail.com>
2025-07-09 21:56:37 +0200

    chore: 🛠️ prettier formatting

--- components/layout/header/Header.tsx
+++ components/layout/header/Header.tsx
@@ -75,10 +75,7 @@ const Header: FC = (): JSX.Element => {
         </div>
 
         <div
-          className={`
-            overflow-hidden transition-all duration-300 ease-in-out lg:hidden
-            ${isMenuOpen ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0'}
-          `}
+          className={`overflow-hidden transition-all duration-300 ease-in-out lg:hidden ${isMenuOpen ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0'} `}
         >
           <Menu
             type={DeviceTypeEnum.Mobile}
