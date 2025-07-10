76282ce83903cf7c91a77059750c2b4f489560cc
Krsiak Daniel <krsiak.daniel@gmail.com>
2025-07-10 20:07:35 +0200

    refactor(years): 👷 skills update

--- lib/utils/constants/urls/externalUrls.ts
+++ lib/utils/constants/urls/externalUrls.ts
@@ -3,7 +3,7 @@ import { ExternalURL } from '@/lib/utils/typeDefinitions/interfaces'
 export const EXTERNAL_URL: ExternalURL = {
   gitHub: 'https://github.com/krsiakdaniel/portfolio-website-krsiak-cz',
   linkedIn: 'https://www.linkedin.com/in/krsiakdaniel/',
-  resumeViewPDF: 'https://drive.google.com/file/d/1DannR10afnADKGXRn4Ji0xJIpscIGVSR/view',
+  resumeViewPDF: 'https://drive.google.com/file/d/1Zw7kGKtEHBHn9mvWpCUTwNVo4jTwR7um/view',
   uptimeMonitorStatus: 'https://status.krsiak.cz',
   uptimeMonitorStatusExternal: 'https://krsiak.betteruptime.com/',
   verseBiblePeter1Chapter3Verse21: 'https://www.bible.com/bible/116/1PE.3.21.NLT',
