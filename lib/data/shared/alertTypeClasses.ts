597a8e2fcd731c7fc1db1b7c26c21e196d61323c
Krsiak Daniel <krsiak.daniel@gmail.com>
2025-08-08 09:40:59 +0200

    feature(daisyui): ✨ new components framework (#633)
    * feature(daisyui): ✨ new components framework
    
    * refactor(note): 🛠️ comment
    
    * fix(alert): 🐛 types and colors

--- lib/data/shared/alertTypeClasses.ts
+++ lib/data/shared/alertTypeClasses.ts
@@ -1,5 +1,7 @@
 import { AlertTypeEnum } from '@/lib/utils/typeDefinitions/enums'
 
+// TODO: review custom colors, update daisyUI theme, or use it as is and remove this file
+
 /**
  * A record that maps each alert type from `AlertTypeEnum` to a string of CSS classes.
  * These classes are used to style alerts based on their type.
@@ -8,6 +10,5 @@ export const alertTypeClasses: Record<AlertTypeEnum, string> = {
   [AlertTypeEnum.Info]: 'border-blue-300 bg-blue-100 text-blue-800',
   [AlertTypeEnum.Success]: 'border-green-300 bg-green-100 text-green-800',
   [AlertTypeEnum.Warning]: 'border-yellow-300 bg-yellow-100 text-yellow-800',
-  [AlertTypeEnum.Danger]: 'border-red-300 bg-red-100 text-red-800',
-  [AlertTypeEnum.Neutral]: 'border-neutral-300 bg-neutral-100 text-neutral-800',
+  [AlertTypeEnum.Error]: 'border-red-300 bg-red-100 text-red-800',
 }
