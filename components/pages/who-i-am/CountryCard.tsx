8792babca61aee6cdd5111f311abd737f26dd33c
Krsiak Daniel <krsiak.daniel@gmail.com>
2026-03-12 13:44:40 +0100

    refactor(about): 👷 country card

--- components/pages/who-i-am/CountryCard.tsx
+++ components/pages/who-i-am/CountryCard.tsx
@@ -2,7 +2,7 @@ import { TravelCountry } from '@/lib/utils/typeDefinitions/interfaces'
 import { getFlagAriaLabel, getVisitCountColor } from './CountryCard.utils'
 
 const CountryCard = ({ country }: { country: TravelCountry }) => (
-  <div className="flex items-center justify-between rounded-lg border border-gray-200 bg-white p-4 transition-shadow duration-300 ease-in-out select-none hover:border-gray-300 hover:shadow-md">
+  <div className="flex items-center justify-between rounded-lg border border-gray-200 bg-white px-4 py-2 transition-shadow duration-300 ease-in-out select-none hover:border-gray-300 hover:shadow-md">
     <div className="flex items-center gap-3">
       <span
         role="img"
