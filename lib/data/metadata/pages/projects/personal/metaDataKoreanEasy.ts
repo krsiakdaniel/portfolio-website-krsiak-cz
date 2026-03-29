4d652e483b0b29f1fa7285a69aed35737fdf9907
Krsiak Daniel <krsiak.daniel@gmail.com>
2026-03-29 11:39:33 +0200

    refactor(social): 👷 open graph images Korean Easy

--- lib/data/metadata/pages/projects/personal/metaDataKoreanEasy.ts
+++ lib/data/metadata/pages/projects/personal/metaDataKoreanEasy.ts
@@ -1,9 +1,14 @@
 import { PAGES_URL } from '@/lib/utils/constants/urls/pageUrls'
 import { createPageMetadata } from '@/lib/utils/helpers/metadata/createPageMetadata'
 
+import krsiakOG from '@/public/images/png/open-graph/pages/personal-projects/korean-easy/korean-easy-og.png'
+import krsiakTwitter from '@/public/images/png/open-graph/pages/personal-projects/korean-easy/korean-easy-twitter.png'
+
 import { META_KOREAN_EASY } from '@/localization'
 
 export const metaDataKoreanEasy = createPageMetadata({
   meta: META_KOREAN_EASY,
   pageUrl: PAGES_URL.personalKoreanEasy,
+  ogImage: krsiakOG,
+  twitterImage: krsiakTwitter,
 })
