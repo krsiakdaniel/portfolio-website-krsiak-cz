0e86fbbfad58421fc03c853a0b1af84c2edbedef
Krsiak Daniel <krsiak.daniel@gmail.com>
2026-04-03 12:44:59 +0200

    refactor(image): 👷 new profile picture

--- components/shared/photo/Photo.tsx
+++ components/shared/photo/Photo.tsx
@@ -5,8 +5,7 @@ import { DATA_TEST_IDS } from '@/__tests__/playwright/lib/utils/constants/ids/da
 
 import openToWorkSM from '@/public/images/webp/photo/linkedin/open-to-work-216x216.webp'
 import openToWorkLG from '@/public/images/webp/photo/linkedin/open-to-work-296x296.webp'
-import krsiakSM from '@/public/images/webp/photo/me/krsiak-daniel-216x216.webp'
-import krsiakLG from '@/public/images/webp/photo/me/krsiak-daniel-296x296.webp'
+import krsiakPhoto from '@/public/images/webp/photo/me/krsiak-daniel-600x600.webp'
 
 import { ImageLoadingEnum } from '@/lib/types/enums'
 import { TEXT } from '@/localization'
@@ -14,13 +13,13 @@ import { TEXT } from '@/localization'
 import { PHOTO_DIMENSIONS } from './Photo.constants'
 import { PhotoProps } from './Photo.types'
 
-const Photo = ({ isPhotoSmall, isOpenToWork, showCaption = true }: PhotoProps) => {
+const Photo = ({ isPhotoSmall = false, isOpenToWork = false, showCaption = false }: PhotoProps) => {
   return (
     <div className="flex flex-col">
       <div className="relative rounded-full bg-black">
         {/* base image */}
         <ImageComponent
-          src={isPhotoSmall ? krsiakSM : krsiakLG}
+          src={krsiakPhoto}
           width={isPhotoSmall ? PHOTO_DIMENSIONS.SMALL.WIDTH : PHOTO_DIMENSIONS.LARGE.WIDTH}
           height={isPhotoSmall ? PHOTO_DIMENSIONS.SMALL.HEIGHT : PHOTO_DIMENSIONS.LARGE.HEIGHT}
           alt={TEXT.nameDanielKrsiak}
